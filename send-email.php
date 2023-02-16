b6ed296b1b7853975992698f719083df8aba9ba9
unknown <ilesanmi3451@gmail.com>
2023-02-16 17:06:39 +0100

    send-email 2

--- send-email.php
+++ send-email.php
@@ -1,28 +1,43 @@
 <?php
-// Get the form data
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get form data
+  $name = strip_tags(trim($_POST["name"]));
+  $name = str_replace(array("\r","\n"),array(" "," "),$name);
+  $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+  $message = trim($_POST["message"]);
 
-// Set the recipient email address
-$to = 'ilesanmi3451@gmail.com';
+  // Check that data was sent
+  if (empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    http_response_code(400);
+    echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+    exit;
+  }
 
-// Set the email subject
-$subject = 'New message from ' . $name;
+  // Set recipient email address
+  $recipient = "ilesanmi3451@gmail.com";
 
-// Set the email message
-$body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+  // Set email subject
+  $subject = "New message from $name";
 
-// Set the email headers
-$headers = "From: $email\r\n";
-$headers .= "Reply-To: $email\r\n";
+  // Set email content
+  $email_content = "Name: $name\n";
+  $email_content .= "Email: $email\n\n";
+  $email_content .= "Message:\n$message\n";
 
-// Send the email
-if (mail($to, $subject, $body, $headers)) {
-  // Redirect to a thank-you page
-  header('Location: thank-you.html');
+  // Set email headers
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+
+  // Send the email
+  if (mail($recipient, $subject, $email_content, $headers)) {
+    http_response_code(200);
+    echo "Thank You! Your message has been sent.";
+  } else {
+    http_response_code(500);
+    echo "Oops! Something went wrong and we couldn't send your message.";
+  }
 } else {
-  // Display an error message
-  echo 'There was an error sending the email.';
+  http_response_code(403);
+  echo "There was a problem with your submission, please try again.";
 }
 ?>
